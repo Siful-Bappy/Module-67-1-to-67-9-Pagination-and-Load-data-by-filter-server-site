eb7eb37044254727fb2dfc30934413cd39fe1798
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-04-29 02:47:59 +0900

    till 3rd video done

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-
 const express = require('express')
-const cors = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middle ware
-app.use(cors());
+// app.use(cors());
+
+const corsConfig = {
+    origin: true,
+    credentials: true,
+    }
+app.use(cors(corsConfig))
+app.options('*', cors(corsConfig))
+
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u10di.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try{
-
+        await client.connect();
+        const productCollection = client.db("emajohn").collection("product");
+        
+        app.get("/product", async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
     }
     finally{
         
     }
 }
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
   res.send('This is my getway for the main route')
