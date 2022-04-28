055d21f284519fb32250d9caa1a44d20b566609f
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-04-28 23:21:16 +0900

    till 2nd video done

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 
 const express = require('express')
 const cors = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u10di.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+
+    }
+    finally{
+        
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('This is my getway for the main route')
