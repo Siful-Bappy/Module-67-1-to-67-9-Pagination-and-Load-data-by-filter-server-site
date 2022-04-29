771b6655b5acdb1c71355c12f15ee6dded11855c
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-04-29 23:38:49 +0900

    till 5th video done

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 // middle ware
-// app.use(cors());
+app.use(cors());
+app.use(express.json());
 
+/* 
 const corsConfig = {
     origin: true,
     credentials: true,
     }
 app.use(cors(corsConfig))
-app.options('*', cors(corsConfig))
-
-app.use(express.json());
+app.options('*', cors(corsConfig)) 
+*/
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u10di.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -28,8 +29,17 @@ async function run() {
         app.get("/product", async(req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
+            // const products = await cursor.limit(76).toArray();
             const products = await cursor.toArray();
             res.send(products);
+          })
+
+        app.get("/productCount", async(req, res) => {
+          const query = {};
+          const cursor = productCollection.find(query);
+          const count = await cursor.count();
+          // because we are sending as string
+          res.send({count});
         })
     }
     finally{
