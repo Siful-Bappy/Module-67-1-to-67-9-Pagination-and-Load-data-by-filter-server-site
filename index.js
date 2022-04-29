3ba7dd237698c661c9e38583743e88f42d5c173d
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-04-30 02:11:20 +0900

    till 7th video done

--- index.js
+++ index.js
@@ -27,17 +27,28 @@ async function run() {
         const productCollection = client.db("emajohn").collection("product");
         
         app.get("/product", async(req, res) => {
+          console.log("query", req.query);
+          const page = parseInt(req.query.page);
+          const size = parseInt(req.query.size);
             const query = {};
             const cursor = productCollection.find(query);
+            let products;
             // const products = await cursor.limit(76).toArray();
-            const products = await cursor.toArray();
+            if(page || size) {
+              products = await cursor.skip(page*size).limit(size).toArray();
+            } else {
+              products = await cursor.toArray();
+            }
             res.send(products);
           })
 
         app.get("/productCount", async(req, res) => {
           const query = {};
           const cursor = productCollection.find(query);
-          const count = await cursor.count();
+          const count = await productCollection.estimatedDocumentCount
+          ();
+          // in future cursor er opre count kora jabe na ti new way te use kora
+          // const count = await cursor.count();
           // because we are sending as string
           res.send({count});
         })
